678c8137407c30c1b666ccfa4bec750870fa7f1f
Dante Soares <dante.m.soares@rice.edu>
2016-01-20 18:47:51 -0600

    Cleaned up the config a bit

--- lita_config.rb
+++ lita_config.rb
@@ -21,18 +21,15 @@ Lita.configure do |config|
   # The adapter you want to connect with. Make sure you've added the
   # appropriate gem to the Gemfile.
   config.robot.adapter = :slack
+
+  ## Set options for the chosen adapter.
   config.adapters.slack.token = ENV['SLACK_TOKEN'] || ''
 
-  ## Example: Set options for the chosen adapter.
-  # config.adapter.username = "myname"
-  # config.adapter.password = "secret"
-
-  ## Example: Set options for the Redis connection.
+  ## Set options for the Redis connection.
   config.redis[:url] = ENV['REDIS_URL']
 
   config.http.port = ENV['PORT'] || 80
 
-  ## Example: Set configuration for any loaded handlers. See the handler's
-  ## documentation for options.
+  ## Set configuration for any loaded handlers. See the handler's documentation for options.
   config.handlers.http_router.token = ENV['HTTP_TOKEN']
 end
