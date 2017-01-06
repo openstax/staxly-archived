07730291456d94ff19e381f86d47aebbe5458e0a
Dante Soares <dante.m.soares@rice.edu>
2017-01-06 10:53:07 -0600

    Leave nickname blank because it does not work properly when set

--- handlers/http/cleverbot.rb
+++ handlers/http/cleverbot.rb
@@ -4,7 +4,7 @@ module Lita
   module Handlers
     module Http
       class Cleverbot
-        CLIENT = ::Cleverbot.new(ENV['CLEVERBOT_USER'], ENV['CLEVERBOT_KEY'], 'staxly')
+        CLIENT = ::Cleverbot.new(ENV['CLEVERBOT_USER'], ENV['CLEVERBOT_KEY'])
 
         def run(command, args)
           CLIENT.say "#{command} #{args}"
